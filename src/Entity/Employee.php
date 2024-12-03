45b164ec23930b5181b4501f0c25af031a2265ae
Quentin Avrain <quentin.avrain@gmail.com>
2024-12-03 12:20:35 +0100

    Finalisation

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: EmployeeRepository::class)]
 #[ORM\HasLifecycleCallbacks]
@@ -18,6 +20,7 @@ class Employee
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column]
